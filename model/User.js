6ed0785ff0a8fdb758269fb015f3c3ffc4f88b25
Abhishek <abhishekrajsingh136@gmail.com>
2023-02-05 21:12:49 +0530

    add-likes

--- model/User.js
+++ model/User.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
     mediaFile:{type:String, require:true},
     userName :{type:String, require:true},
     location :{type:String, require:true},
-    likes :{type:Number, default:50},
+    likes :{type:Number, default:Math.floor(Math.random() * 1001)},
     discription:{type:String, require:true},
     created: {
         type: Date,
